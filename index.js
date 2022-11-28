bc4415fc56efd9306a5cc2716eeaa8931d6abb18
Jyoti <email.jyotisingh13@gmail.com>
2022-11-28 13:13:55 +0530

    completed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // This file is not to be modified. Please ignore this.
 // We will understand all of this later in the course.
 // DO NOT MODIFY THIS FILE
-var alertText = document.getElementById("alert-text");
 
 const express = require('express');
 const path = require('path');
